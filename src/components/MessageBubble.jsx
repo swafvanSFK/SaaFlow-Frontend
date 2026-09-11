669fc437fd88f09544e7b2f1cae0c3c6ad7064ae
swafvanSFK <safuswafvan1@gmail.com>
2026-09-11 21:02:52 +0400

    Working on chat

--- src/components/MessageBubble.jsx
+++ src/components/MessageBubble.jsx
@@ -1,8 +1,17 @@
+import Markdown from "react-markdown"
+import remarkGfm from "remark-gfm"
 
 const MessageBubble = ({role, content}) => {
+
+  const isUser = role === "user"
+
   return (
-    <div>
-        
+    <div className={`flex ${isUser ? "justify-end": "justify-start"}`}>
+        <div className={`max-w-[72%] px-4 py-2.5 rounded-2xl text-[13.5px] leading-relaxed ${isUser ? "bg-linear-to-br from-indigo-500 to-violet-700 text-white rounded-tr-sm": "bg-white/[0.04] border border-white/[0.07] text-slate-200 rounded-tl-sm"}`}>
+          <Markdown remarkPlugins={[remarkGfm]}>
+            {content}
+          </Markdown>
+        </div>
     </div>
   )
 }
